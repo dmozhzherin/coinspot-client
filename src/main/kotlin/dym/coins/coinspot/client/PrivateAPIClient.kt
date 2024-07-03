8d762177c2f7ca3d2691e69acb906b458e3d1d50
dimitrim <dmitrii@darasa.io>
2024-07-03 15:01:46 +0400

    Nonce is generated before signing the request. Request preparation is synchronized, because Coinspot requires nonce to be growing.

--- src/main/kotlin/dym/coins/coinspot/client/PrivateAPIClient.kt
+++ src/main/kotlin/dym/coins/coinspot/client/PrivateAPIClient.kt
@@ -2,20 +2,18 @@ package dym.coins.coinspot.client
 
 import dym.coins.coinspot.api.request.HMACRequest
 import dym.coins.coinspot.api.resource.ResponseMeta
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.headers
-import io.ktor.client.request.invoke
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.HttpMethod
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.net.URL
-import java.util.HexFormat
-import java.util.concurrent.locks.ReentrantLock
+import java.util.*
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-import kotlin.concurrent.withLock
 
 /**
  * @author dym
@@ -24,6 +22,9 @@ import kotlin.concurrent.withLock
 
 private const val HMAC_SHA_512 = "HmacSHA512"
 
+/**
+ * All private clients are constructed with a key and a secret. That means every customer must have their own API client.
+ */
 abstract class PrivateAPIClient(private val apiKey: String, apiSecret: String) : APIClient() {
 
     private val keySpec = SecretKeySpec(apiSecret.toByteArray(), HMAC_SHA_512)
@@ -51,18 +52,25 @@ abstract class PrivateAPIClient(private val apiKey: String, apiSecret: String) :
         }
     }
 
+    /**
+     * Prepare and run the request.
+     * The call is synchronized in an attempt to ensure the correct order of the ever-growing nonce
+     */
     protected suspend fun <T : HMACRequest, P : ResponseMeta, R> callApi(
         url: URL,
         body: T,
         clazz: Class<P>,
         transform: (P) -> R = { it as R }
-    ): R = lock.withLock { prepareRequest(url, body) }
-        .let { httpClient.post(it) }
-        .run {
-            return processResponse(this, clazz, transform)
-        }
+    ): R = lock.withLock {
+        val request = prepareRequest(url, body)
+        coroutineScope { async { httpClient.post(request) } }
+    }.await().run {
+        return processResponse(this, clazz, transform)
+    }
 
+    //The mutex is static and that means all customers are synchronised, which is excessive. I'll think about it later.
     companion object {
-        val lock = ReentrantLock()
+        @JvmStatic
+        val lock = Mutex()
     }
 }
