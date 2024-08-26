9198e1ee529f28bc989ef1a05095852a1933206b
Dmitry Mozhzherin <dmitry.mozhzherin@gmail.com>
2024-08-27 00:36:20 +0400

    Yep, upper case

--- src/main/kotlin/dym/coins/coinspot/domain/AssetType.kt
+++ src/main/kotlin/dym/coins/coinspot/domain/AssetType.kt
@@ -81,7 +81,7 @@ data class AssetType internal constructor(
 
         @JvmStatic
         @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-        fun of(code: String): AssetType = assetTypes.computeIfAbsent(code.uppercase()) { AssetType(code, code, null) }
+        fun of(code: String) = assetTypes.computeIfAbsent(code.uppercase()) { key -> AssetType(key, key, null) }
     }
 
 }
